ac639602ff7654e35becee7049310d2002c0cf27
DracoOmega <draco.omega.dcss.dev@gmail.com>
2025-04-30 21:38:06 -0230

    Interrupt putting on armour when polymorphed (cool3)
    This could lead to weird and invalid equipment situations.
    
    (This does, as a consequence, cause polymorph to interrupt *all* delays,
    but probably suddenly turning into a bat would cause one to lose their
    concentration on many things.)

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -7716,6 +7716,8 @@ bool player::polymorph(int dur, bool allow_immobile)
 
     if (f != transformation::none && transform(dur, f, true))
     {
+        stop_delay(true, true);
+
         transform_uncancellable = true;
         return true;
     }
